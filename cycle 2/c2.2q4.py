1896d3cdfd040872c4c8cd70ab7b7ed1b018bcea
abhaj2 <74650828+abhaj2@users.noreply.github.com>
2022-01-03 10:40:43 +0530

    Create c2.2q4.py

---
+++ cycle 2/c2.2q4.py
@@ -0,0 +1,10 @@
+import numpy as np
+m1 = np.array([[1,4],[2,5]])
+m2 = np.array([[1,4],[2,5]])
+m3 = np.dot(m1,m2) 
+print("multiplication of two matrix")
+print(m3)
+m4=np.array([[5,6],[1,4]])
+m5=np.dot(m3,m4)
+print("multiplication of two matrix with third one")
+print(m5)
