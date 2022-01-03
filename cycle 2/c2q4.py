34b308493ac94aed82fe977cc7ba98d4fb60b965
abhaj2 <74650828+abhaj2@users.noreply.github.com>
2022-01-03 10:33:59 +0530

    Create c2q4.py

---
+++ cycle 2/c2q4.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr = np.arange(1, 11)
+
+print("Total number of elements in the array : \n",arr)
+print("First four elements : \n " ,arr[:4])
+print("Last six elements \n", arr[4:])
+print("Elements between index 2 to 7 : \n ", arr[1:7])
