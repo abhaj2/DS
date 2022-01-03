8e6b0190e164c7e2fe0799d6255e1c481cc14bc0
abhaj2 <74650828+abhaj2@users.noreply.github.com>
2022-01-03 10:40:09 +0530

    Create c2.2q3.py

---
+++ cycle 2/c2.2q3.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+np.random.seed(42)
+
+A = np.random.randint(0, 10, size=(5,6))
+
+B = np.random.randint(0, 10, size=(3,3))
+
+print("Matrix A:\n{}, shape={}\n".format(A, A.shape))
+
+print("Matrix B:\n{}, shape={}\n".format(B, B.shape))
+
+C = A[1:4,2:5] @ B
+
+A[1:4,2:5] = C
+
+print("Matrix A after submatrix multiplication:\n{}, shape={}\n".format(A, A.shape))
